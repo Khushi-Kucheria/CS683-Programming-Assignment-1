b726fd3a01ff73ef4fe7f7060380cdbbccfddb6d
Hamshini08 <hamshini08@gmail.com>
2026-09-06 10:11:21 +0530

    Refactor tile size constants in conv_tile function

--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -3,21 +3,19 @@
 #include "convolution.h"
 #include <algorithm>   
 
-#ifndef TILE_H
-#define TILE_H 32
+#ifndef TILE
+#define TILE 32
 #endif
-#ifndef TILE_W
-#define TILE_W 32
-#endif
+
 void conv_tile(const float* in, float* out, const float* ker,
                int H, int W, int K) {
     const int p = K / 2;
     const int in_stride = W + 2 * p;
-      for (int ty = 0; ty < H; ty += TILE_H) { //H and W are divided into TILE X TILE blocks hence both ty and tx jump by tile size.
+      for (int ty = 0; ty < H; ty += TILE) { //H and W are divided into TILE X TILE blocks hence both ty and tx jump by tile size.
        
-            int oy_end = std::min(ty + TILE_H, H); //tile size might not divide H and W evenly, so in such a case, take the min of ty/tx + tile and the row/col number.
-         for (int tx = 0; tx < W; tx += TILE_W) {
-            int ox_end = std::min(tx + TILE_W, W);
+            int oy_end = std::min(ty + TILE, H); //tile size might not divide H and W evenly, so in such a case, take the min of ty/tx + tile and the row/col number.
+         for (int tx = 0; tx < W; tx += TILE) {
+            int ox_end = std::min(tx + TILE, W);
         
       
               for (int oy = ty; oy < oy_end; ++oy) { //entire block placed in the cache, hence when the kernel matrix traverses, all the block data would not be kicked out and by the time the kernel accesses say the next row, it gets a hit.
