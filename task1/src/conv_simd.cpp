036a588d4d7079e901cc1aaa97ffa685d76b2877
Hamshini08 <hamshini08@gmail.com>
2026-09-07 11:08:04 +0530

    Update conv_simd.cpp

--- task1/src/conv_simd.cpp
+++ task1/src/conv_simd.cpp
@@ -3,14 +3,14 @@ void conv_simd(const float* in, float* out, const float* ker,
     const int p = K / 2;
     const int in_stride = W + 2 * p;
 
-    // Precompute broadcasts once (kernel doesn't change across the whole image)
+   
     static thread_local std::vector<__m256> ker_bcast;
     ker_bcast.resize(K * K);
-    for (int i = 0; i < K * K; ++i) ker_bcast[i] = _mm256_set1_ps(ker[i]);
-
+    for (int i = 0; i < K * K; ++i) ker_bcast[i] = _mm256_set1_ps(ker[i]); //precomputation step.
+//for a particular kernel weight, it stores the same copy across all 8lanes in a 256-bit vector hence avoiding broadcasting the same weight repeatedly.
     for (int oy = 0; oy < H; ++oy) {
         int ox = 0;
-        for (; ox + 8 <= W; ox += 8) {
+        for (; ox + 8 <= W; ox += 8) { //loop over 8 outputs parallely.
             __m256 acc = _mm256_setzero_ps();
             for (int ky = 0; ky < K; ++ky) {
                 for (int kx = 0; kx < K; ++kx) {
